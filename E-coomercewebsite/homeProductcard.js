6a997f2d4d95b3de667fd568d5493cda2d9a11f3
Bhavya <bgprajapati575@gmail.com>
2024-05-23 16:07:14 +0530

    day2

--- E-coomercewebsite/homeProductcard.js
+++ E-coomercewebsite/homeProductcard.js
@@ -1,18 +1,40 @@
-const productContainer = document.querySelector('#productContainer');
-const productTemplate = document.querySelector('#productTemplate');
 
 
+const productContainer = document.querySelector("#productContainer");
+const productTemplate = document.querySelector("#productTemplate");
 
+export const showproductcard = (products) => {
+    if (!products) {
+        return false;
+    }
 
+    products.forEach((curProd) => {
+        const { brand, category, description, id, image, name, price, stock } =
+            curProd;
 
-export const showproductcard = (product) => {
-    if(!product) return;
+        const productClone = document.importNode(productTemplate.content, true);
 
-    product.forEach((currpro)=>{
-        const {brand, category, description, id, image, name, price, stock } = currpro;
-        
-
-    })
-
-
+        productClone.querySelector(".category").textContent = category;
+        productClone.querySelector(".productName").textContent = name;
+        productClone.querySelector(".productImage").src = image;
+        productClone.querySelector(".productStock").textContent = stock;
+        productClone.querySelector(".productPrice").textContent += price;
+        productClone.querySelector(".productDescription").textContent = description;
+        productClone.querySelector(".productActualPrice").textContent = `$${price * 2}`;
+        productContainer.appendChild(productClone);
+    });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// import { addToCart } from "./addToCart";
+// import { homeQuantityToggle } from "./homeQuantityToggle";
